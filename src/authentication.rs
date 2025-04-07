0377e3b9f23ef33bb703d5ffdecc05508923f3be
rustnew <martialwato50@gmail.com>
2025-04-07 05:11:10 +0100

    implementation de l'authentification  avec rust

---
+++ src/authentication.rs
@@ -0,0 +1,213 @@
+use  std::str::FromStr;
+
+use pbkdf2::{
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Pbkdf2,
+};
+use rand_core::{OsRng, RngCore};
+use crate::{
+    errors::{LoginError, SignupError},
+    Database, Random, USER_COOKIE_NAME,
+};
+
+#[derive(Clone , Copy)]
+pub(crate) struct SessionToken(u128);
+
+impl FromStr for SessionToken {
+    type Err = <u128 as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse().map(Self)
+    }
+}
+
+impl SessionToken {
+    pub fn generate_new(random: Random) -> Self {
+
+        let mut u128_pool = [0u8; 16];
+        random.lock().unwrap().fill_bytes(&mut u128_pool);
+        Self(u128::from_le_bytes(u128_pool))
+    }
+
+    pub fn into_cookie_value(self) -> String {
+        // TODO Opportunity for a smaller format that is still a valid cookie value
+        self.0.to_string()
+    }
+
+    pub fn into_database_value(self) -> Vec<u8> {
+        self.0.to_le_bytes().to_vec()
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct User {
+    pub username: String,
+}
+
+#[derive(Clone)]
+pub(crate) struct AuthState(Option<(SessionToken, Option<User>, Database)>);
+
+impl AuthState {
+    pub fn logged_in(&self) -> bool {
+        self.0.is_some()
+    }
+
+    pub async fn get_user(&mut self) -> Option<&User> {
+
+        let (session_token, store, database) = self.0.as_mut()?;
+
+        if store.is_none() {
+            const QUERY: &str =
+                "SELECT id, username FROM users JOIN sessions ON user_id = id WHERE session_token = $1;";
+
+            let user: Option<(i32, String)> = sqlx::query_as(QUERY)
+                .bind(&session_token.into_database_value())
+                .fetch_optional(&*database)
+                .await
+                .unwrap();
+
+            if let Some((_id, username)) = user {
+                *store = Some(User { username });
+            }
+        }
+        store.as_ref()
+    }
+}
+
+pub(crate) async fn new_session(database: &Database, random: Random, user_id: i32) -> SessionToken {
+    const QUERY: &str = "INSERT INTO sessions (session_token, user_id) VALUES ($1, $2);";
+
+    let session_token = SessionToken::generate_new(random);
+
+    let _result = sqlx::query(QUERY)
+        .bind(&session_token.into_database_value())
+        .bind(user_id)
+        .execute(database)
+        .await
+        .unwrap();
+
+    session_token
+}
+
+pub(crate) async fn auth<B>(
+    mut req: http::Request<B>,
+    next: axum::middleware::Next<B>,
+    database: Database,
+) -> axum::response::Response {
+    let session_token = req
+        .headers()
+        .get_all("Cookie")
+        .iter()
+        .filter_map(|cookie| {
+            cookie
+                .to_str()
+                .ok()
+                .and_then(|cookie| cookie.parse::<cookie::Cookie>().ok())
+        })
+        .find_map(|cookie| {
+            (cookie.name() == USER_COOKIE_NAME).then(move || cookie.value().to_owned())
+        })
+        .and_then(|cookie_value| cookie_value.parse::<SessionToken>().ok());
+
+    req.extensions_mut()
+        .insert(AuthState(session_token.map(|v| (v, None, database))));
+
+    next.run(req).await
+}
+
+pub(crate) async fn signup(
+    database: &Database,
+    random: Random,
+    username: &str,
+    password: &str,
+) -> Result<SessionToken, SignupError> {
+    fn valid_username(username: &str) -> bool {
+
+        (1..20).contains(&username.len())
+            && username
+                .chars()
+                .all(|c| matches!(c, 'a'..='z' | '0'..='9' | '-'))
+    }
+
+    if !valid_username(username) {
+        return Err(SignupError::InvalidUsername);
+    }
+
+    const INSERT_QUERY: &str =
+        "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id;";
+
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Hash password to PHC string ($pbkdf2-sha256$...)
+    let password_hash = Pbkdf2.hash_password(password.as_bytes(), &salt);
+
+    let hashed_password = if let Ok(password) = password_hash {
+        password.to_string()
+    } else {
+        return Err(SignupError::InvalidPassword);
+    };
+
+    let fetch_one = sqlx::query_as(INSERT_QUERY)
+        .bind(username)
+        .bind(hashed_password)
+        .fetch_one(database)
+        .await;
+
+    let user_id: i32 = match fetch_one {
+        Ok((user_id,)) => user_id,
+        Err(sqlx::Error::Database(database))
+            if database.constraint() == Some("users_username_key") =>
+        {
+            return Err(SignupError::UsernameExists);
+        }
+        Err(_err) => {
+            return Err(SignupError::InternalError);
+        }
+    };
+
+    Ok(new_session(database, random, user_id).await)
+}
+
+
+pub(crate) async fn login(
+    database: &Database,
+    random: Random,
+    username: &str,
+    password: &str,
+) -> Result<SessionToken, LoginError> {
+    const LOGIN_QUERY: &str = "SELECT id, password FROM users WHERE users.username = $1;";
+
+    let row: Option<(i32, String)> = sqlx::query_as(LOGIN_QUERY)
+        .bind(username)
+        .fetch_optional(database)
+        .await
+        .unwrap();
+
+    let (user_id, hashed_password) = if let Some(row) = row {
+        row
+    } else {
+        return Err(LoginError::UserDoesNotExist);
+    };
+
+    // Verify password against PHC string
+    let parsed_hash = PasswordHash::new(&hashed_password).unwrap();
+    if let Err(_err) = Pbkdf2.verify_password(password.as_bytes(), &parsed_hash) {
+        return Err(LoginError::WrongPassword);
+    }
+
+    Ok(new_session(database, random, user_id).await)
+}
+
+pub(crate) async fn delete_user(auth_state: AuthState) {
+    const DELETE_QUERY: &str = "DELETE FROM users 
+        WHERE users.id = (
+            SELECT user_id FROM sessions WHERE sessions.session_token = $1
+        );";
+
+    let auth_state = auth_state.0.unwrap();
+    let _res = sqlx::query(DELETE_QUERY)
+        .bind(&auth_state.0.into_database_value())
+        .execute(&auth_state.2)
+        .await
+        .unwrap();
+}
---
+++ src/main.rs
@@ -0,0 +1,224 @@
+mod  errors;
+mod authentication;
+mod utils;
+
+use std::sync::{Arc, Mutex};
+use sqlx::Postgres;
+
+use axum::{
+    extract::{Extension, Multipart, Path},
+    middleware,
+    response::{Html, IntoResponse, Redirect},
+    routing::{any, get, post},
+    Router,
+};
+use http::Response;
+
+use authentication::{auth, delete_user, login, signup, AuthState};
+use errors::{LoginError, NoUser, NotLoggedIn, SignupError};
+use pbkdf2::password_hash::rand_core::OsRng;
+use rand_chacha::ChaCha8Rng;
+use rand_core::{RngCore, SeedableRng};
+use shuttle_service::{error::CustomError};
+use shuttle_axum::ShuttleAxum;
+use sqlx::Executor;
+use tera::{Context, Tera};
+use utils::*;
+
+use sqlx::PgPool;
+type Templates = Arc<Tera>;
+type Database = sqlx::PgPool;
+type Random = Arc<Mutex<ChaCha8Rng>>;
+
+const USER_COOKIE_NAME: &str = "user_token";
+const COOKIE_MAX_AGE: &str = "9999999";
+
+
+#[shuttle_runtime::main]
+async fn server(
+  #[shuttle_shared_db::Postgres] pool: PgPool
+) -> ShuttleAxum {
+pool.execute(include_str!("../schema.sql"))
+          .await
+          .map_err(CustomError::new)?;
+ 
+      Ok(get_router(pool).into())
+}
+
+
+pub fn get_router(database: Database) -> Router {
+    let mut tera = Tera::default();
+    tera.add_raw_templates(vec![
+        ("base.html", include_str!("../templates/base.html")),
+        ("index", include_str!("../templates/index.html")),
+        ("signup", include_str!("../templates/signup.html")),
+        ("login", include_str!("../templates/login.html")),
+        ("users", include_str!("../templates/users.html")),
+        ("user", include_str!("../templates/user.html")),
+    ])
+    .unwrap();
+
+    let middleware_database = database.clone();
+    let random = ChaCha8Rng::seed_from_u64(OsRng.next_u64());
+
+    Router::new()
+        .route("/", get(index))
+        .route("/signup", get(get_signup).post(post_signup))
+        .route("/login", get(get_login).post(post_login))
+        .route("/logout", post(logout_response))
+        .route("/delete", post(post_delete))
+        .route("/me", get(me))
+        .route("/user/:username", get(user))
+        .route("/users", get(users))
+        .route("/styles.css", any(styles))
+        .layer(middleware::from_fn(move |req, next| {
+            auth(req, next, middleware_database.clone())
+        }))
+        .layer(Extension(Arc::new(tera)))
+        .layer(Extension(database))
+        .layer(Extension(Arc::new(Mutex::new(random))))
+}
+
+
+async fn index(
+    Extension(current_user): Extension<AuthState>,
+    Extension(templates): Extension<Templates>,
+) -> impl IntoResponse {
+
+    let mut context = Context::new();
+
+    context.insert("logged_in", &current_user.logged_in());
+    context.insert("home_screen", &true);
+    Html(templates.render("index", &context).unwrap())
+}
+
+async fn user(
+    Path(username): Path<String>,
+    Extension(mut auth_state): Extension<AuthState>,
+    Extension(database): Extension<Database>,
+    Extension(templates): Extension<Templates>,
+) -> impl IntoResponse {
+    const QUERY: &str = "SELECT username FROM users WHERE username = $1;";
+
+    let user: Option<(String,)> = sqlx::query_as(QUERY)
+        .bind(&username)
+        .fetch_optional(&database)
+        .await
+        .unwrap();
+
+    if let Some((username,)) = user {
+        let user_is_self = auth_state
+            .get_user()
+            .await
+            .map(|logged_in_user| logged_in_user.username == username)
+            .unwrap_or_default();
+
+
+        let mut context = Context::new();
+        
+        context.insert("username", &username);
+        context.insert("is_self", &user_is_self);
+        Ok(Html(templates.render("user", &context).unwrap()))
+    } else {
+        Err(error_page(&NoUser(username)))
+    }
+}
+
+async fn get_signup(Extension(templates): Extension<Templates>) -> impl IntoResponse {
+    Html(templates.render("signup", &Context::new()).unwrap())
+}
+
+async fn get_login(Extension(templates): Extension<Templates>) -> impl IntoResponse {
+    Html(templates.render("login", &Context::new()).unwrap())
+}
+
+async fn post_signup(
+    Extension(database): Extension<Database>,
+    Extension(random): Extension<Random>,
+    multipart: Multipart,
+) -> impl IntoResponse {
+    let data = parse_multipart(multipart)
+        .await
+        .map_err(|err| error_page(&err))?;
+
+    if let (Some(username), Some(password), Some(confirm_password)) = (
+        data.get("username"),
+        data.get("password"),
+        data.get("confirm_password"),
+    ) {
+        if password != confirm_password {
+            return Err(error_page(&SignupError::PasswordsDoNotMatch));
+        }
+
+        match signup(&database, random, username, password).await {
+            Ok(session_token) => Ok(login_response(session_token)),
+            Err(error) => Err(error_page(&error)),
+        }
+    } else {
+        Err(error_page(&SignupError::MissingDetails))
+    }
+}
+
+async fn post_login(
+    Extension(database): Extension<Database>,
+    Extension(random): Extension<Random>,
+    multipart: Multipart,
+) -> impl IntoResponse {
+    let data = parse_multipart(multipart)
+        .await
+        .map_err(|err| error_page(&err))?;
+
+    if let (Some(username), Some(password)) = (data.get("username"), data.get("password")) {
+        match login(&database, random, username, password).await {
+            Ok(session_token) => Ok(login_response(session_token)),
+            Err(err) => Err(error_page(&err)),
+        }
+    } else {
+        Err(error_page(&LoginError::MissingDetails))
+    }
+}
+
+async fn post_delete(Extension(current_user): Extension<AuthState>) -> impl IntoResponse {
+    if !current_user.logged_in() {
+        return Err(error_page(&NotLoggedIn));
+    }
+
+    delete_user(current_user).await;
+
+    Ok(logout_response().await)
+}
+
+async fn styles() -> impl IntoResponse {
+    Response::builder()
+        .status(http::StatusCode::OK)
+        .header("Content-Type", "text/css")
+        .body(include_str!("../public/styles.css").to_owned())
+        .unwrap()
+}
+
+async fn me(
+    Extension(mut current_user): Extension<AuthState>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    if let Some(user) = current_user.get_user().await {
+        Ok(Redirect::to(&format!("/user/{}", user.username)))
+    } else {
+        Err(error_page(&NotLoggedIn))
+    }
+}
+
+async fn users(
+    Extension(database): Extension<Database>,
+    Extension(templates): Extension<Templates>,
+) -> impl IntoResponse {
+    const QUERY: &str = "SELECT username FROM users LIMIT 100;";
+
+    let users: Vec<(String,)> = sqlx::query_as(QUERY).fetch_all(&database).await.unwrap();
+
+    // This should be a no op right :)
+    let users = users.into_iter().map(|(value,)| value).collect::<Vec<_>>();
+
+    let mut context = Context::new();
+    context.insert("users", &users);
+
+    Html(templates.render("users", &context).unwrap())
+}
